483b5cf104d7a3769766ff6bc0a3d65a548c124b
Viktor Ostafiichuk <ostafiyshyk@gmail.com>
2022-07-21 23:49:16 +0300

    add HW lesson 1

---
+++ lesson_1/main.js
@@ -0,0 +1,81 @@
+let he = 'hello';
+let ow = 'owu';
+let co = 'com';
+let ua = 'ua';
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let num5 = 3.14;
+let num6 = 2.7;
+let num7 = 16;
+let boolean1 = true;
+let boolean2 = false;
+
+
+console.log(he);
+console.log(ow);
+console.log(co);
+console.log(ua);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+console.log(boolean1);
+console.log(boolean2);
+
+alert(he);
+alert(ow);
+alert(co);
+alert(ua);
+alert(num1);
+alert(num2);
+alert(num3);
+alert(num4);
+alert(num5);
+alert(num6);
+alert(num7);
+alert(boolean1);
+alert(boolean2);
+
+document.write(he);
+document.write(ow);
+document.write(co);
+document.write(ua);
+document.write(num1);
+document.write(num2);
+document.write(num3);
+document.write(num4);
+document.write(num5);
+document.write(num6);
+document.write(num7);
+document.write(boolean1);
+document.write(boolean2);
+
+let firstName = 'Viktor';
+let middleName = 'Yaroslavovych';
+let lastName = 'Ostafiichuk';
+
+let person = lastName + firstName + middleName;
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let namePrompt;
+let nameFather;
+let age;
+namePrompt = window.prompt('write your name');
+console.log(namePrompt);
+
+nameFather = window.prompt('write your father name');
+console.log(nameFather);
+
+age = window.prompt('write your age');
+console.log(age);
