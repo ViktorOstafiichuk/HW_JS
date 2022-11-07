19b7242bf12a0c73876e9f6ec7554418f3d176fe
Viktor Ostafiichuk <ostafiyshyk@gmail.com>
2022-11-07 21:48:35 +0200

    add

--- lesson_9/task_5/main.js
+++ lesson_9/task_5/main.js
@@ -17,25 +17,25 @@
 
 //-------------------------------------------------------------------------------
 
-// - Є масив
-let coursesAndDurationArray = [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
-];
+// // - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
 //
 // За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
 //     Завдання робити через цикли.
-// for (let items of coursesAndDurationArray) {
-//     let createDiv = document.createElement('div');
-//     createDiv.classList.add('item');
-//     createDiv.innerHTML = `<h1 class="heading">${items['title']}</h1><p class="description">${items['monthDuration']}</p>`;
-//
-//     document.body.appendChild(createDiv);
-// }
+for (let items of coursesAndDurationArray) {
+    let createDiv = document.createElement('div');
+    createDiv.classList.add('item');
+    createDiv.innerHTML = `<h1 class="heading">${items['title']}</h1><p class="description">${items['monthDuration']}</p>`;
+
+    document.body.appendChild(createDiv);
+}
 
 
 // ------------------------------------------------------------------------------
@@ -134,4 +134,4 @@ let coursesAndDurationArray = [
 // }
 
 
-// createTable(elem, col, row);
+//createTable(elem, col, row)
